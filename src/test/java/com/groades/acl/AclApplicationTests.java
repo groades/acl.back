33dc64a0d273da85995e27a46aae8b4921339b01
Gabriel Roades <gabrielroades@gmail.com>
2024-06-11 19:18:31 -0400

    Initial Commit

---
+++ src/test/java/com/groades/acl/AclApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groades.acl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AclApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
