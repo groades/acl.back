d4900c60723c455fb3b9150b642cc8514c481df8
Gabriel Roades <gabrielroades@gmail.com>
2024-06-13 15:54:30 -0400

    OpenAPI Swagger implemented

--- README.md
+++ README.md
@@ -29,6 +29,30 @@ Stop all daemons running
 $ ./gradlew -stop
 ```
 
+### API Swagger
+
+With the application running open the below url from a browser 
+
+```console
+http://localhost:8080/swagger-ui/index.html
+```
+
+## DATABASE
+The database engine used is H2 in memory DB
+
+To open the H2 portal and manage the Tables, open the url from a browser
+
+```console
+http://localhost:8080/h2-console/
+```
+
+Input the value as below:
+
+![img.png](img.png)
+
+Password: acl
+
+
 ## API
 
 Below is the API endpoint with the input and output. Please note that the application needs to be
@@ -84,4 +108,57 @@ Example output
         "isvalid": true
     }
 ]
+```
+### Task Update
+
+Endpoint
+
+```text
+PUT /api/v1/task
+```
+
+Example of body
+
+```json
+{
+  "id": 1,
+  "description": "My description updated",
+  "isvalid": false
+}
+```
+Example output
+
+```json
+{
+  "id": 1,
+  "description": "My description updated",
+  "created": "2024-06-13T14:39:54.665821",
+  "isvalid": false,
+  "isdeleted": false
+}
+```
+
+### Task Deletion
+
+Endpoint
+
+```text
+PUT /api/v1/task/delete/{id}
+```
+
+Input variable
+
+id: The identification number of the task to delete
+
+
+Example output
+
+```json
+{
+  "id": 1,
+  "description": "My description updated",
+  "created": "2024-06-13T14:39:54.665821",
+  "isvalid": false,
+  "isdeleted": true
+}
 ```
--- src/main/java/com/groades/acl/api/controller/TaskController.java
+++ src/main/java/com/groades/acl/api/controller/TaskController.java
@@ -6,6 +6,7 @@ import com.groades.acl.persistence.entity.TaskEntity;
 import com.groades.acl.api.reponse.TaskResponse;
 import com.groades.acl.api.request.TaskCreateRequest;
 import com.groades.acl.service.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -20,21 +21,25 @@ public class TaskController {
     private final TaskService taskService;
 
     @PostMapping("/create")
+    @Operation(summary = "Create a task")
     public ResponseEntity<TaskResponse> create(
             @Validated @RequestBody TaskCreateRequest request
     ){
         return ResponseEntity.ok(taskService.create(request));
     }
     @GetMapping
+    @Operation(summary = "Get undeleted tasks")
     public ResponseEntity<List<TaskEntity>> getAllTasks(){
 
         return ResponseEntity.ok(taskService.getAll());
     }
     @PutMapping("/delete/{id}")
+    @Operation(summary = "Delete a task")
     public ResponseEntity<TaskResponse> delete(@PathVariable Integer id){
         return ResponseEntity.ok(taskService.delete(id));
     }
     @PutMapping
+    @Operation(summary = "Update a task")
     public ResponseEntity<TaskResponse> update(
             @Validated @RequestBody TaskUpdateRequest request){
         return ResponseEntity.ok(taskService.update(request));
